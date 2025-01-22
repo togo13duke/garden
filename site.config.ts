56a32b94d5c14c4802853b4ab9b95e4ea021020b
Harrison Sanada <togo13duke@gmail.com>
2025-01-23 01:46:15 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ export const CONFIG = {
   description: '学んだこと、考えたこと、思うこと、包み隠さずありのままを綴る場所',
   // heptabase whiteboard id
   whiteboardId:
-    '555813eeb21f6814b66919a0286508d5cb71f62ad2077e5737e2644ef53a34bd',
+    '555813eeb21f6814b66919a0286508d5cb71f62ad2077e5737e2644ef53a34bd'
 };
 
 export const SEO = {
