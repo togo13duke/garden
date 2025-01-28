bfaadf400ab714b9489ff9f1cd0b7d8eeff985e5
Harrison Sanada <harrison_sanada@sudo-apt-update.com>
2025-01-28 21:06:07 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
   // site title
   title: 'garden',
   // site description
-  description: '学んだこと、考えたこと、思うこと、包み隠さずありのままを綴る場所',
+  description: '学んだこと、考えたこと、思うこと、ありのまま、綴る場所',
   // heptabase whiteboard id
   whiteboardId:
     '37512e56635463454b93d67380eac494bb3f1473b49c2c3c6c47e0f18c808e3c',
